ad84315cb41963cfa2c249977980f5ab9de0d163
Derik Thiessen <derikt03@live.com>
2024-12-23 10:57:53 -0500

    new: added (untested) functionality for adding price validation columns to the stockData property

--- TickerPredictions.m
+++ TickerPredictions.m
@@ -5,7 +5,7 @@ classdef TickerPredictions
     properties
         ticker string % holds the ticker symbol
         stockData table % holds all the data for a particular ticker
-        priceValidationTable table % holds the price for each date offset by one day earlier for model comparison
+        numRows double % holds the number of rows where prices are given for the input dataset
         dataStartDate datetime % holds the start date for the input time series
         dataEndDate datetime % holds the end date for the input time series
         securityName string % holds the name of the security
@@ -13,63 +13,148 @@ classdef TickerPredictions
         gicsSubIndustry string % holds the GICS sub-industry
         dateAdded datetime % holds the date that the security was added to the S&P 500
         yearFounded double % holds the year that the security was founded
-
-        % NEED TO DEFINE THE OTHER ATTRIBUTES HERE, WHICH INCLUDE THE 
-        % VARIOUS DESCRIPTIVE COLUMNS FOUND IN THE DATA DUMP
     end
 
     methods
         % Constructor
-        function obj = TickerPredictions(ticker, inputData)
+        function self = TickerPredictions(ticker, inputData, numPeriodsShifts)
             validateattributes(ticker, {'string'}, {})
-            obj.ticker = ticker;
+            self.ticker = ticker;
 
             validateattributes(inputData, {'table'}, {});
-            obj.stockData = inputData;
+            self.stockData = inputData;
 
-            obj.stockData = TickerPredictions.convertDateColumnToDatetime(obj.stockData);
-            obj.dataStartDate = obj.getDataStartDate();
-            obj.dataEndDate = obj.getDataEndDate();
-            obj.securityName = obj.getSecurityName();
-            obj.gicsSector = obj.getGicsSector();
-            obj.gicsSubIndustry = obj.getGicsSubIndustry();
-            obj.dateAdded = obj.getDateAdded();
-            obj.yearFounded = obj.getYearFounded();
+            self.stockData = TickerPredictions.convertDateColumnToDatetime(self.stockData);
+            self.stockData = self.shiftClosePriceBack(numPeriodsShifts);
+            self.numRows = self.getNumRows();
+            self.dataStartDate = self.getDataStartDate();
+            self.dataEndDate = self.getDataEndDate();
+            self.securityName = self.getSecurityName();
+            self.gicsSector = self.getGicsSector();
+            self.gicsSubIndustry = self.getGicsSubIndustry();
+            self.dateAdded = self.getDateAdded();
+            self.yearFounded = self.getYearFounded();
+        end
+
+        % Grabs the number of rows in the stockData
+        function numRows = getNumRows(self)
+            numRows = height(self.stockData);
         end
 
         % Grabs the start date of the time series
-        function startDate = getDataStartDate(obj)
-            startDate = obj.stockData.Date(1);
+        function startDate = getDataStartDate(self)
+            startDate = self.stockData.Date(1);
         end
 
         % Grabs the end date of the time series
-        function endDate = getDataEndDate(obj)
-            endDate = obj.stockData.Date(end);
+        function endDate = getDataEndDate(self)
+            endDate = self.stockData.Date(end);
         end
 
         % Grabs the security name
-        function securityName = getSecurityName(obj)
-            securityName = obj.stockData.Security(1);
+        function securityName = getSecurityName(self)
+            securityName = self.stockData.Security(1);
         end
 
         % Grabs the GICS Sector
-        function gicsSector = getGicsSector(obj)
-            gicsSector = obj.stockData.GICSSector(1);
+        function gicsSector = getGicsSector(self)
+            gicsSector = self.stockData.GICSSector(1);
         end
 
         % Grabs the GICS Sub-Industry
-        function gicsSubIndustry = getGicsSubIndustry(obj)
-            gicsSubIndustry = obj.stockData.GICSSub_Industry(1);
+        function gicsSubIndustry = getGicsSubIndustry(self)
+            gicsSubIndustry = self.stockData.GICSSub_Industry(1);
         end
 
         % Grabs the date added
-        function dateAdded = getDateAdded(obj)
-            dateAdded = obj.stockData.DateAdded(1);
+        function dateAdded = getDateAdded(self)
+            dateAdded = self.stockData.DateAdded(1);
         end
 
         % Grabs the year founded
-        function yearFounded = getYearFounded(obj)
-            yearFounded = obj.stockData.Founded(1);
+        function yearFounded = getYearFounded(self)
+            yearFounded = self.stockData.Founded(1);
+        end
+
+        function stockData = shiftClosePriceBack(self, numPeriodsShifts)
+                
+            % Throw an error if no arguments were given
+            if nargin == 0
+                error('Function requires a tabular input of stock price data.');
+            
+            % Use a default of only a one row shift if none is passed
+            elseif nargin == 1
+                numPeriodsShifts = [1];
+            
+            % If an array of shifts is passed by the user, validate that all shifts
+            % are appropriate given the input data    
+            else
+                numPeriodsShifts = self.validateInputNumPeriodsShifts(numPeriodsShifts);
+            end
+
+            % Holds the stockData so it can be continually updated
+            stockData = self.stockData;
+
+            % Initializes a zeros array to hold the newly created column names
+            priceValidationColumnNames = zeros(1, numel(numPeriodsShifts));
+
+            % Add each value in numPeriodsShifts as an offset column to the closing price column
+            for i = 1:numel(numPeriodsShifts)
+                value = numPeriodsShifts(i);
+                [stockData, newColumnName] = TickerPredictions.addPriceValidationColumn(stockData, value);
+                priceValidationColumnNames(i) = newColumnName;
+            end
+        end
+        
+        % Ensures that the input array for numPeriodsShifts is valid;
+        % if it is valid, returns the array in ascending order
+        function numPeriodsShifts = validateInputNumPeriodsShifts(self, numPeriodsShifts)
+            
+            % Throw an error if there are duplicate values in the array
+            if numel(numPeriodsShifts) ~= numel(unique(numPeriodsShifts))
+                error('MATLAB:InvalidValue', ...
+                'Input array for numPeriodsShifts cannot have duplicate values.');
+            end
+
+            for value = numPeriodsShifts
+                
+                % Throw an error if the value is not numeric
+                if ~isnumeric(value)
+                    errorString = sprintf(...
+                        'All inputs for numPeriodsShifts should be numeric. Received: %s', ...
+                        mat2str(value));
+
+                    error('MATLAB:InvalidType', errorString);
+                
+                % Throw an error if the value is not an integer
+                elseif value ~= floor(value)
+                    errorString = sprintf(...
+                        'All inputs for numPeriodsShifts should be integers. Received: %s', ...
+                        mat2str(value));
+
+                    error('MATLAB:InvalidValue', errorString);
+
+                % Throw an error if the value is not at least 1
+                elseif value < 1
+                    errorString = sprintf(...
+                        'All inputs for numPeriodsShifts should be at least 1. Received: %s', ...
+                        mat2str(value));
+
+                    error('MATLAB:InvalidValue', errorString);
+                
+                % Throw an error if the value is greater than the number of rows in the dataset
+                elseif value >= self.numRows
+                    errorString = sprintf(...
+                        'All inputs for numPeriodsShifts should be less than the number of rows. Num rows: %s. Input received: %s', ...
+                        mat2str(self.numRows), mat2str(value));
+
+                    error('MATLAB:InvalidValue', errorString);
+                end
+            
+                % If all checks passed, return the array sorted in ascending order
+                numPeriodsShifts = sort(numPeriodsShifts);
+
+            end
         end
     end
 
@@ -95,6 +180,14 @@ classdef TickerPredictions
                 % Removes the timezone from the datetime
                 stockData.Date.TimeZone = '';
             end
+        end
+
+        % Adds a column to stockData which contains the price of the next periodOffset unit of time
+        function [stockData, columnName] = addPriceValidationColumn(stockData, periodOffset)
+            columnName = sprintf('Offset_%s', periodOffset);
+            closingPriceValues = stockData.Close;
+            offsetData = [closingPriceValues((periodOffset + 1) : end); NaN(numRowsShift, 1)];
+            stockData.(columnName) = offsetData;
         end
     end
 end
